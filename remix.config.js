55c0966a19689a6f3bd22676c48178385286c056
Håkon Meyer Tørnquist <hakon.tornquist@vy.no>
2022-12-29 16:34:16 +0100

    feat: Deploy to Vercel

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+  serverBuildTarget: "vercel",
+  devServerPort: 8002,
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
 };
